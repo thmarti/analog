f244f554f4fdc7a4177d96f18b8723f431f9ed7c
Brandon Roberts <robertsbt@gmail.com>
2023-04-06 16:57:38 -0500

    chore: add StackBlitz config

--- packages/create-analog/template-angular-v15/vite.config.ts
+++ packages/create-analog/template-angular-v15/vite.config.ts
@@ -12,7 +12,11 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [
+    analog({
+      ssr: false, // Disabled for StackBlitz
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
